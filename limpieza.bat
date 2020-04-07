82ab10f6d4be61f848547c11541758ab6caf3255
Alejandro <54931487+slipialex@users.noreply.github.com>
2020-04-07 16:05:17 -0500

    Update limpieza.bat

--- limpieza.bat
+++ limpieza.bat
@@ -1,21 +1,18 @@
 @echo off
 ECHO "limpieza de carpeta de descargas"
+PAUSE
 IF NOT exist %UserProfile%\Documents\Descargas (
-    ECHO Se crea carpeta para hacer respaldo de las descargas
     cd %UserProfile%\Documents
     md Descargas
     cd Descargas
-    ECHO Se Copian Archivos
-    FORFILES /p "%UserProfile%\Downloads" /d -10 /m *.pdf -c "cmd /c copy @file %UserProfile%\Documents\Descargas"
-    ECHO se Abre Carpeta
-    TIMEOUT /t 5
+    ECHO Copiando Archivos
+    FORFILES /p "%UserProfile%\Downloads" /d -10 /m *.* -c "cmd /c xcopy @file %UserProfile%\Documents\Descargas /s /e /y"
     START .
 ) ELSE (
-    ECHO Ya existe una carpeta
-    FORFILES /p "%UserProfile%\Downloads" /d -10 /m *.pdf -c "cmd /c copy @file %UserProfile%\Documents\Descargas"
-    ECHO se Abre Carpeta
+    ECHO Copiando Archivos
     CD %UserProfile%\Documents\Descargas
-    TIMEOUT /t 5
+    FORFILES /s /p "%UserProfile%\Downloads" /d -10 /m * -c "cmd /c xcopy @FILE %UserProfile%\Documents\Descargas /e /y" 
     START .
+    PAUSE
 )
 end
